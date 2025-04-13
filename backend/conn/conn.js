d0abc56ef823ed0a3a2b9d031db160a4b928e177
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-04-14 01:45:55 +0530

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config(); 
 
 const conn  = mongoose.connect(process.env.MongoDB_URI).then(() => {
   console.log("Database Connected.");
